9966969043940cf58a41e8492281f8ab06614724
bimapw4 <bimapw4@gmail.com>
2022-07-02 18:34:54 +0700

    add service for send email with gmail

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import (
 	"adoptme/config"
 	"adoptme/routes"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gopkg.in/gomail.v2"
 )
 
 func main() {
@@ -15,7 +18,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+	mail()
 	config.Connect()
 	config.Migrate(&model.User{})
 
@@ -24,4 +27,27 @@ func main() {
 	routes.Router(router)
 
 	router.Run()
+}
+
+func mail() {
+	mailer := gomail.NewMessage()
+	mailer.SetHeader("From", os.Getenv("CONFIG_SENDER_NAME"))
+	mailer.SetHeader("To", "bimapw4@gmail.com")
+	mailer.SetHeader("Subject", "Test mail")
+	mailer.SetBody("text/html", "<b>have a nice day</b>")
+
+	port, _ := strconv.Atoi(os.Getenv("CONFIG_SMTP_PORT"))
+	dialer := gomail.NewDialer(
+		os.Getenv("CONFIG_SMTP_HOST"),
+		port,
+		os.Getenv("CONFIG_AUTH_EMAIL"),
+		os.Getenv("CONFIG_AUTH_PASSWORD"),
+	)
+
+	err := dialer.DialAndSend(mailer)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Println("Mail sent!")
 }
